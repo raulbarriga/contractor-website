789cb25742c273eecdc5cf58a8465245ff193bea
Raul Barriga <barrigaraulaug@gmail.com>
2021-08-26 23:11:24 -0700

    Fixed an issue with the new svgs. Found the fix at: https://github.com/airbnb/babel-plugin-inline-react-svg/issues/76#issuecomment-854438227

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import Head from 'next/head';
+import Head from "next/head";
 
 import "../styles/reset.css";
 import "slick-carousel/slick/slick.css";
@@ -9,16 +9,18 @@ import "../styles/ModalCarousel.css";
 import "../styles/index.scss";
 
 const MyApp = ({ Component, pageProps }) => {
-    return (<>
-    <Head>
-    <meta
-            name="viewport"
-            content="width=device-width,initial-scale=1"
-          />
-<title>JB & B Construction, Inc. | Serving the Greater San Francisco Bay Area and Its Surroundings.</title>
-    </Head>
-    <Component {...pageProps} />
-    </>)
-  }
-  
-  export default MyApp
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <title>
+          JB & B Construction, Inc. | Serving the Greater San Francisco Bay Area
+          and Its Surroundings.
+        </title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+};
+
+export default MyApp;
