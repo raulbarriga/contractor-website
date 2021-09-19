62075592d46659aaa4c808b149605f09d2b0088f
Raul Barriga <barrigaraulaug@gmail.com>
2021-09-19 16:15:19 -0700

    Fixed the axios url since I had changed the url for the deployed site.

--- axios/index.js
+++ axios/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 let url = "";
-process.env.NODE_ENV === 'development' ? (url = "http://localhost:3000/api/contactForm") : process.env.NODE_ENV === 'production' ? (url = "https://tio-jorge-website.vercel.app/api/contactForm") : console.log("process.env.NODE_ENV: ", process.env.NODE_ENV);
+process.env.NODE_ENV === 'development' ? (url = "http://localhost:3000/api/contactForm") : process.env.NODE_ENV === 'production' ? (url = "https://jbb-construction-inc-9q0w5mxfp-raulbarriga.vercel.app/api/contactForm") : console.log("process.env.NODE_ENV: ", process.env.NODE_ENV);
 
 export const sendForm = async (contactData) => {
     try {
