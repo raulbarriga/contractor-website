d832937e11b24236f5866a5e970e69595e4b23c4
Raul Barriga <barrigaraulaug@gmail.com>
2021-09-22 15:53:38 -0700

    Added github actions folder and placed the secret in the remote repo settings too.

--- pages/api/contactForm.js
+++ pages/api/contactForm.js
@@ -2,6 +2,8 @@ const nodemailer = require("nodemailer");
 const { google } = require("googleapis");
 require("dotenv").config();
 
+// const automate = require("../cronJobs/cronJob.js");
+
 // export this from the api route to remove api unnecessary warning
 export const config = {
   api: {
@@ -49,26 +51,7 @@ async function createTransporter() {
   return transporter;
 }
 
-// async function sendMail(firstName, lastName, email, message, phone) {
-//   try {
-//     // Generate the accessToken
-//     // const accessToken = await OAuth2Client.getAccessToken();
-
-//     const response = await transporter.sendMail(
-//       mailOptions,
-//       (error, response) => {
-//         error ? console.log(error) : console.log(response);
-//         transporter.close();
-//       }
-//     );
-
-//     return response;
-//   } catch (error) {
-//     return error;
-//   }
-// }
-
-// Next.js expect only 1 export default function for req, res.
+// Next.js expects only 1 export default function for req, res.
 export default function handler(req, res) {
   // this will be the contact form details sent from the contact form
   const firstName = req.body["first-name"];
@@ -125,74 +108,13 @@ export default function handler(req, res) {
       break;
   }
 }
+// function automation () {
+  // Schedule tasks to be run on the server.
+const automate = cron.schedule('* * * * * *', function() {
+  console.log('running a task every 5 seconds');
+}, {
+  scheduled: false
+});
+// }
 
-/*
-* copy
-async function sendMail(firstName, lastName, email, message, phone) {
-  createTransporter();
-
-  try {
-    // Generate the accessToken
-    const accessToken = await OAuth2Client.getAccessToken();
-console.log("accessToken: ", accessToken);
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        type: "OAuth2",
-        user: process.env.MAIL_USERNAME,
-        clientId: process.env.OAUTH_CLIENTID,
-        clientSecret: process.env.OAUTH_CLIENT_SECRET,
-        refreshToken: process.env.OAUTH_REFRESH_TOKEN,
-        accessToken,
-      },
-      tls: {
-        // thanks to: https://medium.com/@nickroach_50526/sending-emails-with-node-js-using-smtp-gmail-and-oauth2-316fe9c790a1
-        rejectUnauthorized: false, // this made my request token work!
-      },
-    });
-
-    let htmlOutput = `
-  <p>You have a new estimate request.</p>
-  <h3>Contact Details</h3>
-  <ul>
-  <li>
-  <b>First Name: </b>${firstName}
-  </li>
-  <li>
-  <b>Last Name: </b>${lastName}
-  </li>
-  <li>
-  <b>Email: </b>${email}
-  </li>
-  <li>
-  <b>Phone Number: </b>${phone}
-  </li>
-  </ul>
-  <h3>Message</h3>
-  <p>${message}</p>
-  `;
-
-    let mailOptions = {
-      from: `"${firstName} ${lastName}" <${email}>`, // the sender's name and email
-      to: process.env.MAIL_USERNAME, // the company's email address
-      subject: "Quote Request",
-      html: htmlOutput,
-    };
-
-    const response = await transporter.sendMail(mailOptions, (error, response) => {
-      error ? console.log(error) : console.log(response);
-      transporter.close();
- });
-
-    return response;
-  } catch (error) {
-    return error;
-  }
-}
-
-
-* POST code:
-* sendMail(firstName, lastName, email, message, phone)
-        .then((result) => res.json(result))
-        .catch((error) => res.json(error.message));
-*/
+automate.start();
---
+++ pages/api/cronJob.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+
+export default async function handler(req, res) {
+  switch (req.method) {
+  // case 'GET':
+  //   //...
+  //   break
+  case "POST":
+    try {
+      const { authorization } = req.headers;
+
+      if (authorization === `Bearer ${process.env.CRON_JOB_JWT}`) {
+          console.log("hello world from the cron job console log!")
+        res.status(200).json({ success: true , message: "hello world from the cron job json message!" });
+      } else {
+        res.status(401).json({ message: "Token incorrect or missing." });
+      }
+    } catch (err) {
+      res.status(500).json({ statusCode: 500, message: err.message });
+    }
+    break;
+  default:
+    res.setHeader("Allow", "POST");
+    res.status(405).end(); //Method Not Allowed
+    break;
+}
+}
+
+
