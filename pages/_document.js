a34dcdb7e09926c9fc2c8b9acb4b86eadd9cd5a4
Raul Barriga <barrigaraulaug@gmail.com>
2021-10-08 08:56:33 -0700

    Added seo keywords and a new meta description. Might need to change up h1's and h2's around as well.

--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,12 @@ class MyDocument extends Document {
           <meta name="HandheldFriendly" content="true" />
           <meta
             name="description"
-            content="General contractor serving the Greater San Francisco Bay Area. We also serve its surrounding regions."
+            content="Get a free estimate for kitchen and bathroom remodeling, electrical, interior and exterior painting, plumbing, decks, driveway, roofing, apartment or house renovations, drywall or new construction."
+          />
+          <meta
+            name="keywords"
+            content="kitchen remodeling, bathroom remodeling, interior painting, exterior painting, drywall, roofing, apartment renovations, house renovations, new construction, driveway, decks, plumbing, electricity, remodeling contractors near me, licensed general contractor, general contractor near me, general contractor, home improvement contractors, construction companies near me, bay area general contractors, san francisco general contractors, san jose general contractors, fremont general contractors, tracy general contractors, hillsborough general contractors, home renovations, floor installation, house makeover, home makeover, apartment renovation,metal roofing, commercial roofing contractors, commercial roofing companies near me
+          "
           />
           <link
             href="https://fonts.googleapis.com/css2?family=Barlow:wght@100;200;400;500&family=Nunito:wght@300;400&family=Playfair+Display:wght@400;700&display=swap"
