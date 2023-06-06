0b9ef7865f52c6042cc22f4232cf091b5e5f2253
Raul Barriga <barrigaraulaug@gmail.com>
2023-06-05 20:30:23 -0700

    Tested the site works as expected & it does. Made fixes to any breaks & now works as before. Has the latest package updates as well.

--- components/Testimonials.jsx
+++ components/Testimonials.jsx
@@ -7,6 +7,8 @@ import { isMobile } from "react-device-detect";
 import LeftBtn from "../public/svgs/chevron-left.svg";
 import RightBtn from "../public/svgs/chevron-right.svg";
 import DoubleQuotes from "../public/svgs/ri-double-quotes.svg";
+import { quotesArr } from "./data/testimonialsData";
+
 // const wrap = dynamic(
 //    () => {
 //     const { wrap } = import("popmotion");
@@ -23,47 +25,6 @@ const Testimonials = () => {
   useEffect(() => {
     setMobile(isMobile);
   }, [setMobile]);
-
-  const quotesArr = [
-    {
-      key: 1,
-      client: "Kowdle Prasad",
-      quote:
-        "In regard to the work JB & B Construction completed, I can definitely say it was very professionally done. My property manager also said he thought the work was very well executed. I thank you very much for doing such professional work. It gives me great pleasure to use your company for my future re-modeling/construction needs. I look forward to working with you in the future. You guys are GREAT!",
-    },
-    {
-      key: 2,
-      client: "Erika Revels",
-      quote:
-        "I am a property manager for 12 apartment complexes. JB & B Construction did all the interior renovation and exterior painting. They were very professional and did an amazing job! We are ready to hire them for another property. Highly recommend!",
-    },
-    {
-      key: 3,
-      client: "Nick Romero",
-      quote:
-        "JB & B Construction remodeled my home's interior completely. New kitchen, new bathrooms, new flooring and baseboards, new doors, light fixtures, and drywall, they even custom-made new window sills for all my windows! Service was done at a reasonable price, and with the utmost professionalism. Their team made every effort to accommodate requests and to go above and beyond! Thank you!",
-    },
-    {
-      key: 4,
-      client: "Johnathan Rivera",
-      quote:
-        "Very reliable and dependable. But most importantly great quality work and very productive. I am a construction superintendent and we are definitely using this company on many more projects.",
-    },
-    {
-      // A REAL CLIENT of JB & B
-      key: 5,
-      client: "Amanda Taycon Properties",
-      quote:
-        "Jorge and his team remodeled my house and they went above and beyond to ensure that everything was perfect by the time they were finished. Their attention to detail and precision is almost unseen in today's workmanship and you can tell they pride themselves in doing A quality work. I get compliments everytime someone comes over on the way my house looks. I cannot recommend JB & B enough. From their prices, services, and quality you will not go wrong with hiring them for any project you need.",
-    },
-    {
-      // A REAL CLIENT of JB & B
-      key: 5,
-      client: "Esperanza",
-      quote:
-        "JB & B has amazing service and quality work. My go to company for all projects!",
-    },
-  ];
 
   const variants = {
     enter: (direction) => {
@@ -95,15 +56,9 @@ const Testimonials = () => {
   const objIndex = wrap(0, length, page);
 
   const handleSetClick = (index) => {
-    if (page === 0 && index === length - 1) {
-      setPage([length - 1, -1]);
-    } else if (page === length - 1 && index === 0) {
-      setPage([0, 1]);
-    } else if (index > page) {
-      setPage([page + 1, 1]);
-    } else if (index < page) {
-      setPage([page - 1, -1]);
-    }
+    const currentIndex = page;
+    const direction = index > currentIndex ? 1 : -1;
+    setPage([index, direction]);
   };
 
   const paginate = (newDirection) => {
@@ -208,7 +163,7 @@ const Testimonials = () => {
       </button>
       <div className="text">
         <div className="wrapper">
-          <AnimatePresence initial={false} custom={direction} exitBeforeEnter>
+          <AnimatePresence initial={false} custom={direction} mode="wait">
             {unique()}
           </AnimatePresence>
         </div>
